152e74d1fed946bcc1e9822285498bbc64c81570
Max Iliashenko <uncle.ho.max@gmail.com>
2020-05-05 21:31:22 -0400

    Navigation tree template

--- BlazorMDI/Server/Controllers/HomeController.cs
+++ BlazorMDI/Server/Controllers/HomeController.cs
@@ -1,6 +1,6 @@
-﻿using System.Collections.Generic;
-using BlazorMDI.Shared.Navigation;
+﻿using BlazorMDI.Shared.Navigation;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 
 namespace BlazorMDI.Server.Controllers
 {
@@ -16,20 +16,45 @@ namespace BlazorMDI.Server.Controllers
             
             for (var i = 1; i < 10; i++)
             {
-                var an = new NavTreeNode { Title = "Application " + i, HasNavigation = false };
-                an.SubNodes.Add(new NavTreeNode { Title = "Processes", HasNavigation = false });
+                var an = new NavTreeNode { Title = "Application " + i, HasNavigation = false, IsTopLevel = true};
+                an.SubNodes.Add(new NavTreeNode
+                {
+                    Title = "Processes", Icon="fad fa-cogs", HasNavigation = false,
+                    Description = "Processes allow to create a flow of data among forms"
+                });
                 an.SubNodes.Add(new NavTreeNode
                 {
                     Title = "Forms",
                     HasNavigation = false,
                     SubNodes = new List<NavTreeNode>
                     {
-                    new NavTreeNode {Title = "Form Management", HasNavigation = false},
-                    new NavTreeNode {Title = "Form Behaviours", HasNavigation = false},
-                    new NavTreeNode {Title = "Form Actions", HasNavigation = false}
+                    new NavTreeNode
+                    {
+                        Title = "Form Management", Icon="fad fa-pen-square", HasNavigation = false,
+                        Description = "Create and design application forms"
+                    },
+                    new NavTreeNode
+                    {
+                        Title = "Form Behaviours", Icon="fad fa-sliders-h-square", HasNavigation = false,
+                        Description = "Create and edit Form Behaviours. Behaviour defines how and where form is using"
+                    },
+                    new NavTreeNode
+                    {
+                        Title = "Form Categories", Icon="fad fa-share-alt-square", HasNavigation = false,
+                        Description = "Form Categories allow you to create menu hierarchies"
+                    },
+                    new NavTreeNode
+                    {
+                        Title = "Form Actions", Icon="fad fa-caret-square-right", HasNavigation = false,
+                        Description = "Form Actions allow you to use complex logic (code and rules) on forms"
+                    }
                 }
                 });
-                an.SubNodes.Add(new NavTreeNode { Title = "Entities", HasNavigation = false, Icon = "fad fa-puzzle-piece"});
+                an.SubNodes.Add(new NavTreeNode
+                {
+                    Title = "Entities", HasNavigation = false, Icon = "fad fa-puzzle-piece",
+                    Description = "Entities store user's data"
+                });
                 res.Add(an);
             }
 
